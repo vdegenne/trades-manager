1dff504b5cc149f8420fffd7535216b4bd467315
vdegenne <vdegenne@gmail.com>
2021-03-22 06:48:23 +0100

    red dark color fix

--- app.js
+++ app.js
@@ -15941,7 +15941,7 @@ function formatOutputPrice(value, quote, sign = false) {
 }
 function outputPriceTemplate(value, quote, light = false) {
     const green = light ? '#00ff00' : '#05cb05';
-    const red = light ? '#ff6d62' : '#ff0000';
+    const red = light ? '#f44336' : '#ff0000';
     return html `
   <span style="color:${value === 0 ? 'initial' : value > 0 ? green : red};font-weight:500">${formatOutputPrice(value, quote, true)}</span>
   `;
--- src/util.ts
+++ src/util.ts
@@ -70,7 +70,7 @@ export function formatOutputPrice (value: number, quote: string, sign = false) {
 
 export function outputPriceTemplate (value: number, quote: string, light = false) {
   const green = light ? '#00ff00' : '#05cb05';
-  const red = light ? '#ff6d62' : '#ff0000'
+  const red = light ? '#f44336' : '#ff0000'
 
   return html`
   <span style="color:${value === 0 ? 'initial' : value > 0 ? green : red};font-weight:500">${formatOutputPrice(value, quote, true)}</span>
